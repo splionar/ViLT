7cc87559b818fc6ad6c01889edebc5ec23fa2a67
Stefan Lionar <31429575+splionar@users.noreply.github.com>
2021-09-08 14:16:07 +0200

    Update config.py

--- vilt/config.py
+++ vilt/config.py
@@ -20,9 +20,10 @@ def _loss_names(d):
 def config():
     exp_name = "vilt"
     seed = 0
-    datasets = ["coco", "vg", "sbu", "gcc"]
+    datasets = ["coco"]
+    #, "vg", "sbu", "gcc"]
     loss_names = _loss_names({"itm": 1, "mlm": 1})
-    batch_size = 4096  # this is a desired batch size; pl trainer will accumulate gradients when per step batch is smaller.
+    batch_size = 512  # this is a desired batch size; pl trainer will accumulate gradients when per step batch is smaller.
 
     # Image setting
     train_transform_keys = ["pixelbert"]
@@ -94,9 +95,10 @@ def env_dandelin():
 @ex.named_config
 def task_mlm_itm():
     exp_name = "mlm_itm"
-    datasets = ["coco", "vg", "sbu", "gcc"]
+    datasets = ["coco"]
+    #, "vg", "sbu", "gcc"]
     loss_names = _loss_names({"itm": 1, "mlm": 1})
-    batch_size = 4096
+    batch_size = 512
     max_epoch = 10
     max_image_len = 200
 
